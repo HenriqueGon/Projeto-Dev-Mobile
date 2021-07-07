5bdea529ccb7d49aee7b81c192325f7b5a4dd7f5
Henrique Gonçalves de Oliveira <henriquegon.o@hotmail.com>
2021-07-07 20:55:42 -0300

    gestão de estado
    Criado a gestão de estado de um widget e os métodos para atualizar a lista, editar e excluir itens

--- Controle de estoque/lib/app/injection.dart
+++ Controle de estoque/lib/app/injection.dart
@@ -2,9 +2,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:teste/app/dataBase/daos/productDaoImpl.dart';
 import 'package:teste/app/domain/interfaces/productDao.dart';
+import 'package:teste/app/domain/services/productService.dart';
 
 setupInjection() {
   GetIt getIt = GetIt.I;
 
   getIt.registerSingleton<ProductDAO>(ProductDAOImpl());
+  getIt.registerSingleton<ProductService>(ProductService());
 }
---
+++ Controle de estoque/lib/app/view/productListBack.dart
@@ -0,0 +1,37 @@
+//@dart=2.9
+import 'package:flutter/cupertino.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart';
+import 'package:teste/app/app.dart';
+import 'package:teste/app/domain/entities/product.dart';
+import 'package:teste/app/domain/services/productService.dart';
+
+part 'productListBack.g.dart'; 
+
+class ProductListBack = _ProductListBack with _$ProductListBack;
+
+abstract class _ProductListBack with Store {
+  var _service = GetIt.I.get<ProductService>();
+  
+  @observable
+  Future<List<Product>> list;
+
+  _ProductListBack() {
+    refreshList();
+  }
+
+  @action
+  refreshList([dynamic value]) {
+    list = _service.find();
+  }
+
+  goToForm(BuildContext context, [Product product]) {
+    Navigator.of(context).pushNamed(MyApp.NEWPRODUCT, arguments: product).then(refreshList);
+  }
+
+  remove(int id) {
+    _service.remove(id);
+
+    refreshList();
+  } 
+}
